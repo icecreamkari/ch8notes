f8aec17f2bbf5ea015d105fbbfadc11bbe9a722e
icecreamkari <yaykari@gmail.com>
2017-03-23 15:09:23 -0500

    labs

--- ch8notes/Recursive_starter.java
+++ ch8notes/Recursive_starter.java
@@ -29,81 +29,108 @@ public class Recursive_starter
 
         System.out.println(numbers(5));
         System.out.println(rev_numbers(5));
+        System.out.println(prime(5));
+        System.out.println(prime(16));
 
 
     }//end of main
 
 
-public static int btp(int a, int b)
-{
-
-
-    if (b == 0)
-        return 1;
-    else
-        return a * btp(a, b - 1);
-}
-
-public static int foo(int x)
-{
-    if (x > 0)
-        foo(x+1);
-
-        return 0;
-}
-
-public static void reverse_print(String s)
-{
-    
-    if (s.length() > 1)
+    public static int btp(int a, int b)
     {
-        reverse_print(s.substring(1));
+    
+    
+        if (b == 0)
+            return 1;
+        else
+            return a * btp(a, b - 1);
     }
     
-    System.out.print(s.charAt(0));
-
-}
-
-/**
- * prints numbers from n to 0
- */
-public static String numbers(int num)
-{
-    if (num == 1)
-        return "1";
-    else
+    public static int foo(int x)
     {
-        return num + ""  + numbers(num-1);
+        if (x > 0)
+            foo(x+1);
+    
+            return 0;
     }
-}
-
-/**
- * prints numbers from 0 to n
- */
-public static String rev_numbers(int num)
-{
-    if (num == 0)
+    
+    /**
+     * prints a string in rever
+     */
+    public static void reverse_print(String s)
+    {
+        
+        if (s.length() > 1)
+        {
+            reverse_print(s.substring(1));
+        }
+        
+        System.out.print(s.charAt(0));
+    
+    }
+    
+    /**
+     * prints numbers from n to 0
+     */
+    public static String numbers(int num)
+    {
+        if (num == 1)
+            return "1";
+        else
+        {
+            return num + ""  + numbers(num-1);
+        }
+    }
+    
+    /**
+     * prints numbers from 0 to n
+     */
+    public static String rev_numbers(int num)
+    {
+         if (num == 0)
+        {
+            reverse_print(Integer.toString(num-1));
+        }
+        
         return Integer.toString(num);
-    else
-    {
-        return 0 + "" + numbers(num-(num-1));
+    
     }
-}
-
-public static int mystery (int num)
-{
-    //System.out.println(num );
-    if (num == 1)
-        return 1;
-    else
+    
+    /**
+     * checks if a number is prime
+     */
+    public static boolean prime(int num)
     {
-
-        return num + mystery(num-1);
-
+        for (int i = 2; i < num; i++)
+        {
+            if (num % i == 0)
+                return false;
+        }
+        return true;
     }
-
-
-}
+    
+    /**
+     * fibonacci sequence
+     */
+    public static void fibonacci(int num)
+    {
+        
+    }
+    
+    public static int mystery (int num)
+    {
+        //System.out.println(num );
+        if (num == 1)
+            return 1;
+        else
+        {
+    
+            return num + mystery(num-1);
+    
+        }
+    
+    
+    }
 
 
 }// end of class
